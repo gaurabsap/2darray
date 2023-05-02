b16464aba245616dfab064800ccaa421c86748bb
Gaurab sapkota <iamgaurabsap20@gmail.com>
2023-05-02 08:41:48 +0545

    Add project files.

---
+++ 2darray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+ArrayList names = new ArrayList() { "gaurab", "shyam","sugam","anjil"};
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+} 
+
+
+
+for (int i = 0; i < names.Count; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+
+//string[] name = new string[1] {"gaurab"};
